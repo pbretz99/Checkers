e1b0e7a60e63859faf5a92ab404d1369e291447f
Philip Bretz <pbretz99@gmail.com>
2022-04-14 16:49:05 -0500

    Stable display of available moves

--- script.js
+++ script.js
@@ -24,19 +24,20 @@ function pieceCSS(pieceElement, row, column, pieceCount, color, borderColor) {
 
 //Return 1 if [row, column] is on the "diagonal" to form the checkerboard
 function onDiagonal(row, column) {
-     let isOnDiagonal = 0;
+     let isOnDiagonal = false;
      if ((row % 2 == 0) & (column % 2 == 0)) {
-          isOnDiagonal = 1;
+          isOnDiagonal = true;
      } else if ((row % 2 == 1) & (column % 2 == 1)) {
-          isOnDiagonal = 1;
+          isOnDiagonal = true;
      }
      return isOnDiagonal;
 }
 
+
 //Initial piece set-up: 0 for no piece, 1 for player 1, and 2 for player 2
 function initialPieceSetUp(row, column) {
      let player = 0;
-     if (onDiagonal(row, column) == 1) {
+     if (onDiagonal(row, column)) {
           if (row <= 2) {
                player = 1;
           } else if (row >= 5) {
@@ -57,8 +58,12 @@ function createTile (row, column, tileCount) {
      return {element: newTileElement,
           id: `tile${tileCount}`,
           row: row, column: column,
+          pieces: [],
           shade: function () {
-               colorSwitch(this.element, color, altColor);
+               this.element.style.backgroundColor = altColor;
+          },
+          unshade: function () {
+               this.element.style.backgroundColor = color;
           }};
 }
 
@@ -82,7 +87,40 @@ function createPiece (tile, pieceCount, player) {
           borderColor = "gray";
      }
      pieceCSS(newPieceElement, tile.row, tile.column, pieceCount, color, borderColor);
-     return {element: newPieceElement, tile: tile, id: newPieceElement.id, player: player};
+     return {element: newPieceElement, tile: tile, id: newPieceElement.id, player: player, selected: false};
+}
+
+//Get positions diagonally adjacent to [row, column]
+function diagonalPositions(row, column) {
+     let positions = [];
+     for (let i = -1; i < 2; i+=2) {
+          for (let j = -1; j < 2; j+=2) {
+               positions.push([row+i, column+j]);
+          }
+     }
+     return positions;
+}
+
+//Piece selection/de-selection event wrapper
+function selectPieceWrapper(moveTiles, piece, allTiles, allPieces) {
+     if (!piece.selected) {selectPiece(moveTiles, piece, allTiles, allPieces);}
+     else {deselectPiece(moveTiles, piece);}
+}
+
+//Piece selecting
+function selectPiece(moveTiles, piece, allTiles, allPieces) {
+     for (tile of allTiles) {tile.unshade();}
+     for (otherPiece of allPieces) {otherPiece.selected = false;}
+     //document.getElementById("testing").innerHTML += `selected ${piece.id}<br>`;
+     for (tile of moveTiles) {tile.shade();}
+     piece.selected = true; 
+}
+
+//Piece de-selecting
+function deselectPiece(moveTiles, piece) {
+     //document.getElementById("testing").innerHTML += `de-selected ${piece.id}<br>`;
+     for (tile of moveTiles) {tile.unshade();}
+     piece.selected = false;
 }
 
 //Initialize board and pieces
@@ -91,6 +129,7 @@ window.onload = function () {
      const board = {
           
           element: document.getElementById("board"),
+          dimensions: [8, 8],
           tiles: [],
           pieces: [],
           players: [{id: "player1"}, {id: "player2"}],
@@ -102,8 +141,8 @@ window.onload = function () {
           
           initializeTiles: function () {
                let tileCount = 0;
-               for (let row = 0; row < 8; row++) {
-                    for (let column = 0; column < 8; column++) {
+               for (let row = 0; row < this.dimensions[0]; row++) {
+                    for (let column = 0; column < this.dimensions[1]; column++) {
                          this.tiles[tileCount] = new createTile(row, column, tileCount);
                          this.element.appendChild(this.tiles[tileCount].element);
                          tileCount += 1;
@@ -113,8 +152,7 @@ window.onload = function () {
 
           initializePieces: function () {
                let pieceCount = 0;
-               for (t in this.tiles) {
-                    let tile = this.tiles[t];
+               for (tile of this.tiles) {
                     if (initialPieceSetUp(tile.row, tile.column) != 0) {
                          let player = this.players[0];
                          if (initialPieceSetUp(tile.row, tile.column) == 2) {
@@ -122,38 +160,64 @@ window.onload = function () {
                          }
                          this.pieces[pieceCount] = new createPiece(tile, pieceCount, player);
                          this.element.appendChild(this.pieces[pieceCount].element);
-                         //this.pieces[pieceCount] = {id: `piece${pieceCount}`};
+                         tile.pieces.push(this.pieces[pieceCount]);
                          pieceCount += 1;
                     }
                }
           },
 
-          printTiles: function () {
-               let testBox = document.getElementById("testing"),
-               text = "Positions: ";
-               for (t in this.tiles) {
-                    let tile = this.tiles[t];
-                    text += ` (${tile.row}, ${tile.column})`;
+          availableMoves: function (piece) {
+               let moves = [], potentialPos=diagonalPositions(piece.tile.row, piece.tile.column);
+               for (pos of potentialPos) {
+                    let validMove = true;
+                    if (!this.onBoard(pos[0], pos[1])) {validMove = false;}
+                    else if (this.occupied(pos[0], pos[1])) {validMove = false;}
+                    else if ((piece.player.id == "player1") & (pos[0] < piece.tile.row)) {validMove = false;}
+                    else if ((piece.player.id == "player2") & (pos[0] > piece.tile.row)) {validMove = false;}
+                    if (validMove) {moves.push(this.accessTile(pos[0], pos[1]));}
                }
-               testBox.innerHTML = text;
+               return moves;
           },
 
-          printPieces: function () {
-               let testBox = document.getElementById("testing"),
-               text = "Names: ";
-               for (p in this.pieces) {
-                    let piece = this.pieces[p];
-                    text += ` (${piece.id})`;
+          playerMovesShow: function (player) {
+               let allTiles = this.tiles, allPieces = this.pieces;
+               for (let piece of this.pieces) {
+                    if (piece.player == player) {
+                         let moves = this.availableMoves(piece);
+                         if (moves.length > 0) {
+                              piece.element.addEventListener("click", function(){selectPieceWrapper(moves, piece, allTiles, allPieces)});
+                         }
+                    }
                }
-               testBox.innerHTML = text;
-          }
+          },
+
+          onBoard: function (row, column) {
+               let isOnBoard = true;
+               if (row < 0) {isOnBoard = false;}
+               else if (column < 0) {isOnBoard = false;}
+               else if (row >= this.dimensions[0]) {isOnBoard = false;}
+               else if (column >= this.dimensions[1]) {isOnBoard = false;}
+               return isOnBoard;
+          },
+
+          occupied: function (row, column) {
+               let isOccupied = false, tile = this.accessTile(row, column);
+               if (tile.pieces.length > 0) {isOccupied = true;}
+               return isOccupied;
+          },
+
+          accessTile: function(row, column) {
+               if (this.onBoard(row, column)) {
+                    let index = row * this.dimensions[1] + column;
+                    return this.tiles[index];
+               } else {
+                    return 0;
+               }
+          },
 
      }
      
-     board.initializeTiles();
-     board.initializePieces();
-     //board.printTiles();
-     //board.printPieces();
-     board.pieces[9].element.addEventListener("click", function(){board.tiles[25].shade(); board.tiles[27].shade()})
-
+     board.initialize();
+     board.playerMovesShow(board.players[0]);
+     
 }
